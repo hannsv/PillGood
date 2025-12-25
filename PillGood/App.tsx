e1c61c7f03447d2bbf1b0244e84905ded92b01ad
KimEunA97 <kimeuna9712@gmail.com>
2025-12-25 18:11:19 +0900

    [UI] 메인화면 틀 잡기

--- PillGood/App.tsx
+++ PillGood/App.tsx
@@ -1,21 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { API_KEY } from "@env";
+import BottomNavBar from "./src/components/nav/BottomNavBar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  console.log("API Key:", API_KEY);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        {/* <StatusBar style="auto" /> */}
+        {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+        <BottomNavBar />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
   },
 });
---
+++ PillGood/src/screens/PillListScreen.tsx
@@ -0,0 +1,11 @@
+function PillListScreen() {
+  return (
+    <div>
+      <h1>Pill List</h1>
+      {/* Additional UI components and logic will go here */}
+    </div>
+  );
+}
+
+export default PillListScreen;
+import React from "react";
